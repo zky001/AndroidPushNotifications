3aeb5782a33d5e7c54e66b11628a00504d9352a7
chenj zhang <1134386961@qq.com>
2015-12-08 18:03:12 +0800

    android
    MQTT trans

---
+++ gen/com/blueweb/androidpushnotifications/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.blueweb.androidpushnotifications;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
